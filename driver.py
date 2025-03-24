3702b78fc47d6d054705f3c1f8e3c8560054e300
Arsal Hussain <sahussain2004@gmail.com>
2025-03-24 13:11:43 -0500

    Fixed the python command

--- driver.py
+++ driver.py
@@ -8,8 +8,8 @@ def main():
 
     logging_file = sys.argv[1]
 
-    logger = subprocess.Popen(["python", "logger.py", logging_file], stdin=subprocess.PIPE, encoding="utf8")
-    encryptor = subprocess.Popen(["python", "encrypt.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, encoding="utf8")
+    logger = subprocess.Popen(["python3", "logger.py", logging_file], stdin=subprocess.PIPE, encoding="utf8")
+    encryptor = subprocess.Popen(["python3", "encrypt.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, encoding="utf8")
 
     history = []
 
